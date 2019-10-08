2797a13c2a116c7e487b34ca6a2da2005d586206
Antonio <andelic96@gmail.com>
2019-10-08 14:24:27 +0200

    Added gtest

--- test/memory/tensorData.cpp
+++ test/memory/tensorData.cpp
@@ -1,17 +1,8 @@
 #include "memory/tensorData.hpp"
+#include "gtest/gtest.h"
 
-#include <iostream>
-
-int main()
+TEST(tensorDataTest, size)
 {
-    math::memory::TensorData< double, 2, 3, 4, 6 > test;
-
-    std::cout << "Test size: " << test.size() << std::endl;
-    std::cout << "Test dimension number: " << test.dimensionNum() << std::endl;
-    std::cout << "Test member size: " << test[ 0 ][ 2 ].size() << std::endl;
-
-    math::memory::TensorData< int, 3 > vector;
-    std::cout << "Vector size: " << vector.size() << std::endl;
-    std::cout << "vector member: " << vector[ 0 ] << std::endl;
-
+    math::memory::TensorData< double, 2, 1, 3 > test;
+    ASSERT_EQ( test.size(), 2 );
 }
