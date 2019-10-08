851ebddd904cd3f2d16efa8ab3d29d603a8aa4ea
Antonio <andelic96@gmail.com>
2019-10-08 18:20:40 +0200

    Defined tensor types

--- include/memory/tensorData.hpp
+++ include/memory/tensorData.hpp
@@ -8,50 +8,39 @@ namespace math
 namespace memory
 {
 
-template < typename T, int LEAD_SIZE, int... SIZES >
+template < typename T, int SIZE = 0 >
 class TensorData
 {
 private:
-    std::array < TensorData< T, SIZES... >, LEAD_SIZE > data_;
-public:
-    TensorData< T, SIZES... > const& operator[]( std::size_t ix ) const noexcept
-    {
-        return data_[ ix ];
-    }
-
-    std::size_t size() const noexcept 
-    {
-        return LEAD_SIZE;
-    }
-
-    std::size_t dimensionNum() const noexcept
-    {
-        return sizeof...( SIZES ) + 1;
-    }
-};
-
-template <typename T, int LEAD_SIZE>
-class TensorData< T, LEAD_SIZE >
-{
-private:
-    std::array< T, LEAD_SIZE > data_;
+    std::array < T, SIZE  > data_;
 public:
     TensorData() : data_{ T{} }
     {}
 
-    T operator[]( std::size_t ix ) const noexcept
+    T const& operator[]( std::size_t ix ) const noexcept
     {
         return data_[ ix ];
     }
 
-    std::size_t size() const noexcept 
+    T& operator[]( std::size_t ix ) noexcept
     {
-        return LEAD_SIZE;
+        return data_[ ix ];
     }
+};
 
-    std::size_t dimensionNum() const noexcept
+template< typename T >
+class TensorData< T >
+{
+private:
+    T data_;
+
+public:
+    TensorData() : data_{ T{} }
+    {}
+
+    T get() const noexcept
     {
-        return 1;
+        return data_;
     }
 };
 
--- include/tensor.hpp
+++ include/tensor.hpp
@@ -7,13 +7,16 @@
 namespace math
 {
 
+template< typename T, int... SIZES >
+class Tensor;
+
 template< typename T, int LEAD_SIZE, int... SIZES >
-class Tensor
+class Tensor< T, LEAD_SIZE, SIZES... >
 {
     static_assert( std::is_arithmetic_v< T >, "Tensor can hold only arithmetic types!" );
 
 private:
-    memory::TensorData< T, LEAD_SIZE, SIZES... > data_;
+    memory::TensorData< Tensor< T,  SIZES... >, LEAD_SIZE > data_;
 
 public:
     std::size_t size() const noexcept 
@@ -26,10 +29,39 @@ public:
         return sizeof...( SIZES ) + 1;
     }
 
-    auto& operator[]( std::size_t ix ) const noexcept
+    auto& operator[]( std::size_t ix ) noexcept
+    {
+        return data_[ ix ];
+    }
+
+    auto const& operator[]( std::size_t ix ) const noexcept
     {
         return data_[ ix ];
     }
 };
+
+// scalar tensor
+template< typename T >
+class Tensor< T >
+{
+    static_assert( std::is_arithmetic_v< T >, "Tensor can hold only arithmetic types!" );
+
+private:
+    memory::TensorData< T > data_;
+
+public:
+    T get() const noexcept
+    {
+        return data_.get();
+    }
+
+    std::size_t dimensionNum() const noexcept
+    {
+        return 0;
+    }
+};
+
+template< typename T >
+using Scalar = Tensor< T >;
 
 } // namespace math
