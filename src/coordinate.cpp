5b4495068efcbfa70452415e51e030dc1be97bb1
Antonio <andelic96@gmail.com>
2019-10-14 21:42:22 +0200

    Removed old files

--- src/coordinate.cpp
+++
@@ -1,7 +0,0 @@
-#include "coordinate.hpp"
-
-std::ostream& operator<<( std::ostream& os, Coordinate const& coordinate )
-{
-    os << '(' << coordinate.first() << ", " << coordinate.second() << ")";
-    return os;
-}
